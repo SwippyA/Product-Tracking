aedc0ef63b264c53e872c0d1a51379ad11cab540
SwippyA <shubhnkarswain16@gmail.com>
2024-05-21 21:34:36 +0530

    product routing is done

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -7,17 +7,28 @@ const generateTrackingNumber = () => "TN" + Math.floor(Math.random() * 1000000);
 
 const getAllProducts = asyncHandler(async (req, res) => {
   const products = await Product.find();
+  if (!products.length > 0) {
+    res.json(new ApiResponse(200, products, "there are no Products "));
+  }
   res.json(new ApiResponse(200, products, "Products retrieved successfully"));
 });
 
 const getProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const id = req.params.id;
+  if (!id) {
+    throw new ApiError(404, "id not found");
+  }
+  const product = await Product.findById(id);
+
   if (!product) throw new ApiError(404, "Product not found");
   res.json(new ApiResponse(200, product, "Product retrieved successfully"));
 });
 
 const createProduct = asyncHandler(async (req, res) => {
   const { name, status, location } = req.body;
+  if ([name, status, location].some((field) => field?.trim() === "")) {
+    throw new ApiError(400, "All fields are required");
+  }
   const trackingNumber = generateTrackingNumber();
 
   const product = new Product({ name, status, location, trackingNumber });
@@ -44,10 +55,9 @@ const updateProductById = asyncHandler(async (req, res) => {
 });
 
 const deleteProductById = asyncHandler(async (req, res) => {
-  const product = await Product.findById(req.params.id);
+  const product = await Product.findByIdAndDelete(req.params.id);
   if (!product) throw new ApiError(404, "Product not found");
 
-  await product.remove();
   res.json(new ApiResponse(200, {}, "Product deleted successfully"));
 });
 
